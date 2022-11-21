3d75bc42b91f882ecb29335eafeac3f6d05ba5b3
ghchfolio <84805077+ghchfolio@users.noreply.github.com>
2022-11-20 19:21:30 -0600

    converted loading chart template to component (#2)

---
+++ src/app/shared/components/loading-chart/loading-chart.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-loading-chart',
+  templateUrl: './loading-chart.component.html',
+  styleUrls: ['./loading-chart.component.scss']
+})
+export class LoadingChartComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
