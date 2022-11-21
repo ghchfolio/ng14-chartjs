f123fa7af52aae12861cd6971625e1d6aa97c9d4
chinds <chris.hinds@istation.com>
2022-11-21 09:28:05 -0600

    added loading spinner component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { BarChartComponent } from './shared/components/bar-chart/bar-chart.component';
 import { LoadingChartComponent } from './shared/components/loading-chart/loading-chart.component';
+import { LoadingSpinnerComponent } from './shared/components/loading-spinner/loading-spinner.component';
 
 @NgModule({
     declarations: [
         AppComponent,
         BarChartComponent,
         LoadingChartComponent,
+        LoadingSpinnerComponent,
     ],
     imports: [
         BrowserModule
