1b2d68bba0a6cb069ca4aa749bb18b10a27eb582
ghchfolio <ghchfolio@gmail.com>
2022-11-20 19:06:12 -0600

    converted loading chart template to component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { BarChartComponent } from './shared/components/bar-chart/bar-chart.component';
+import { LoadingChartComponent } from './shared/components/loading-chart/loading-chart.component';
 
 @NgModule({
     declarations: [
         AppComponent,
         BarChartComponent,
+        LoadingChartComponent,
     ],
     imports: [
         BrowserModule
---
+++ src/app/shared/components/loading-chart/loading-chart.component.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { LoadingChartComponent } from './loading-chart.component';
+
+describe('LoadingChartComponent', () => {
+  let component: LoadingChartComponent;
+  let fixture: ComponentFixture<LoadingChartComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ LoadingChartComponent ]
+    })
+    .compileComponents();
+
+    fixture = TestBed.createComponent(LoadingChartComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
