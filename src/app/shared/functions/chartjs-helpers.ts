9a6f1dfa8ae7fdb4af36cb0bdf11c7077b90a8cb
brewzk1 <brewzkee1@gmail.com>
2022-11-25 00:40:50 -0600

    added reverse diag

--- src/app/shared/functions/chartjs-helpers.ts
+++ src/app/shared/functions/chartjs-helpers.ts
@@ -10,14 +10,44 @@ export function CreateDiagonalPattern(fgColor = 'gray', bgColor = 'darkgray') {
         c.fillStyle = bgColor;
         c.fillRect(0, 0, pattern.width, pattern.height);
         c.strokeStyle = fgColor;
+
         c.beginPath();
         c.moveTo(2, 0);
         c.lineTo(10, 8);
         c.stroke();
+
         c.beginPath();
         c.moveTo(0, 8);
         c.lineTo(2, 10);
         c.stroke();
+        return c.createPattern(pattern, 'repeat');
+    }
+
+    return 'lightblue';
+}
+
+export function CreateReverseDiagonalPattern(fgColor = 'gray', bgColor = 'darkgray') {
+    const pattern = document.createElement('canvas');
+    pattern.width = 10;
+    pattern.height = 10;
+
+    let c = pattern.getContext('2d');
+
+    if (c !== null) {
+        c.fillStyle = bgColor;
+        c.fillRect(0, 0, pattern.width, pattern.height);
+        c.strokeStyle = fgColor;
+
+        c.beginPath();
+        c.moveTo(0, 10);
+        c.lineTo(10, 0);
+        c.stroke();
+
+        c.beginPath();
+        c.moveTo(10, 0);
+        c.lineTo(0, 10);
+        c.stroke();
+
         return c.createPattern(pattern, 'repeat');
     }
 
