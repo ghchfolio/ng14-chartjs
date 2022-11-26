b04f09aed035f1d15ea3c7bc3c6afd48638628fa
ghchfolio <ghchfolio@gmail.com>
2022-11-25 20:00:58 -0600

    removed unused imports

--- src/app/services/sales-data.service.ts
+++ src/app/services/sales-data.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
-import { BehaviorSubject, catchError, Observable, of, throwError } from 'rxjs';
-import { HttpClient, HttpErrorResponse } from '@angular/common/http';
+import { BehaviorSubject, catchError, throwError } from 'rxjs';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
     providedIn: 'root'
