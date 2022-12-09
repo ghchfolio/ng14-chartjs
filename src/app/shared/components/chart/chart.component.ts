2ed6463dad764259ba3ae42cd16e850b851d8366
ghchfolio <ghchfolio@gmail.com>
2022-12-08 22:41:51 -0600

    added clickable bar chart

--- src/app/shared/components/chart/chart.component.ts
+++ src/app/shared/components/chart/chart.component.ts
@@ -1,5 +1,6 @@
 import { Component, ElementRef, Input } from '@angular/core';
 import { Chart } from 'node_modules/chart.js/auto';
+import { fromEvent, Subscription } from 'rxjs';
 
 @Component({
     selector: 'app-chart',
@@ -14,18 +15,40 @@ export class ChartComponent {
     chart: any = {};
     config: any = {};
     error: any = {};
+    canvasEl: any;
+    ctx: any;
+    onClickSub: Subscription = new Subscription();
 
     constructor(private elementRef: ElementRef) { }
 
     createChart(config: any) {
-        const canvasEl = this.elementRef.nativeElement.querySelector(`#${this.id}`);
+        this.canvasEl = this.elementRef.nativeElement.querySelector(`#${this.id}`);
+        this.ctx = this.canvasEl?.getContext('2d');
+        this.config = config;
 
-        this.config = { ...config };
-        if (this.config.data !== undefined) this.chart = new Chart(canvasEl, this.config);
+        if (this.config.data !== undefined) this.chart = new Chart(this.ctx, this.config);
+
+        this.onClickSub = fromEvent(this.canvasEl, 'click')
+            .subscribe((val: any) => {
+                let evt = event;
+                const points = this.chart.getElementsAtEventForMode(evt, 'nearest', { intersect: true }, true);
+                if (points.length) {
+                    const firstPoint = points[0];
+                    const label = this.chart.data.labels[firstPoint.index];
+                    const value = this.chart.data.datasets[firstPoint.datasetIndex].data[firstPoint.index];
+                    console.log(firstPoint, label, value)
+                }
+            })
     }
 
     showError(error: any) {
         this.error = error;
     }
 
+    onDestroy() {
+        this.onClickSub.unsubscribe();
+    }
+
+    // https://www.chartjs.org/docs/latest/developers/api.html
+    // https://www.youtube.com/watch?v=dIFLeum2WQY&list=PLnuit5448DojGFLD7hvAffu6jYBQZerhi&index=1&t=2s
 }
