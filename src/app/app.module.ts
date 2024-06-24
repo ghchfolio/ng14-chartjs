f9882f29674e27f74722d0b2c968b10755101fde
ghchfolio <ghchfolio@gmail.com>
2024-06-24 13:54:14 -0500

    update proj to v18

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,24 +1,17 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { HttpClientModule } from '@angular/common/http'
+import { provideHttpClient, withInterceptorsFromDi } from '@angular/common/http'
 
 import { AppComponent } from './app.component';
 import { ChartComponent } from './shared/components/chart/chart.component';
 import { LoadingChartComponent } from './shared/components/loading-chart/loading-chart.component';
 import { LoadingChartSpinnerComponent } from './shared/components/loading-chart-spinner/loading-chart-spinner.component';
 
-@NgModule({
-    declarations: [
+@NgModule({ declarations: [
         AppComponent,
         ChartComponent,
         LoadingChartComponent,
         LoadingChartSpinnerComponent,
     ],
-    imports: [
-        BrowserModule,
-        HttpClientModule
-    ],
-    providers: [],
-    bootstrap: [AppComponent]
-})
+    bootstrap: [AppComponent], imports: [BrowserModule], providers: [provideHttpClient(withInterceptorsFromDi())] })
 export class AppModule { }
